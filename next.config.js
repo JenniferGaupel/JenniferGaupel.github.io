be65f3cac500008d944d752246610216ee57156d
Jennifer Gaupel <jennifergaupel@gmail.com>
2023-03-14 14:28:01 -0400

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  images: {
+    unoptimized: true
+  },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
